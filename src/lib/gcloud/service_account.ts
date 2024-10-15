7fae707dae0618f977db88a3e5570af27fdeaf04
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2024-10-15 16:55:17 +0200

    fix: fix bucket permissions

--- src/lib/gcloud/service_account.ts
+++ src/lib/gcloud/service_account.ts
@@ -1,9 +1,11 @@
 import { interpolate } from '@pulumi/pulumi';
 
 import { ServiceAccountData } from '../../model/google/service_account_data';
-import { dnsConfig } from '../configuration';
+import { backupBucketId, dnsConfig } from '../configuration';
 import { createIAMMember } from '../google/iam/member';
 import { createGCPServiceAccountAndKey } from '../util/google/service_account_user';
+
+import { createGCSIAMMember } from './gcs_iam_member';
 
 /**
  * Creates the Google Cloud service account for Traefik.
@@ -11,8 +13,19 @@ import { createGCPServiceAccountAndKey } from '../util/google/service_account_us
  * @returns {ServiceAccountData} the generated service account
  */
 export const createServiceAccount = (): ServiceAccountData => {
-  const iam = createGCPServiceAccountAndKey('mail', {
-    roles: ['roles/storage.objectUser'],
+  const iam = createGCPServiceAccountAndKey('mail', {});
+
+  iam.serviceAccount.email.apply((email) => {
+    createGCSIAMMember(
+      backupBucketId,
+      `serviceAccount:${email}`,
+      'roles/storage.objectAdmin',
+    );
+    createGCSIAMMember(
+      backupBucketId,
+      `serviceAccount:${email}`,
+      'roles/storage.legacyBucketReader',
+    );
   });
 
   createIAMMember(
